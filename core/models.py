73db455bb5ae21d1e60bd34cdde4ade2179e2486
gustavotrentinidev <gustavotrentini22@gmail.com>
2022-07-01 09:28:28 -0300

    autor e livro

--- core/models.py
+++ core/models.py
@@ -10,3 +10,17 @@ class Editora(models.Model):
     site = models.URLField()
     def __str__(self):
         return self.nome
+
+class Autor(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, blank=False)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    def __str__(self):
+        return self.id, self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey()
